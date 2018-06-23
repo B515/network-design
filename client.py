3938cec249551c5a51e072fa5099c41a0103ae4e
WH_hunter <814188639@qq.com>
2018-06-23 18:59:23 +0800

    json

--- client.py
+++ client.py
@@ -2,6 +2,8 @@
 # 客户端（测试用）
 import socket
 import threading
+import json
+import time
 
 outString = ''
 inString = ''
@@ -9,26 +11,32 @@ nick = ''
 encoding = 'utf-8'
 # ip = "123.207.164.148"  # 正式服务器
 ip = "123.206.13.211"  # 测试服务器
+# ip = "127.0.0.1"
 PORT = 8964
 
 
 def deal_out(sock):
     global nick, outString
     while True:
-        outString = input()
-        outString = nick + ':' + outString
-        sock.send(outString.encode(encoding))
+        content = input()
+        outString = content
+        msg = {'Object': 'all', 'FromUser': 'nick', 'CreateTime': time.strftime("%H:%M:%S", time.localtime()),
+               'MsgType': 'text', 'Content': content}
+        jmsg = json.dumps(msg)
+        sock.send(jmsg.encode(encoding))
 
 
 def deal_in(sock):
     global inString
     while True:
         try:
-            inString = sock.recv(1024).decode(encoding)
-            if not inString:
+            jmsg = sock.recv(1024).decode(encoding)
+            if not jmsg:
                 break
+            msg = json.loads(jmsg)
+            inString = msg['Content']
             if outString != inString:
-                print(inString)
+                print('[' + msg['CreateTime'] + ']' + msg['FromUser'] + ':' + inString)
         except:
             break
 
@@ -37,7 +45,9 @@ nick = input('请输入昵称：')
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((ip, PORT))
 print('成功连入服务器(' + ip + ')')
-sock.send(nick.encode(encoding))
+login = {'nick': nick}
+jlogin = json.dumps(login)
+sock.send(jlogin.encode(encoding))
 
 thin = threading.Thread(target=deal_in, args=(sock,))
 thin.start()
--- server.py
+++ server.py
@@ -2,53 +2,64 @@
 # 服务端
 import socket
 import threading
+import json
+import time
+from collections import defaultdict
 
 
 def client_thread_in(conn, nick):
-    global data
+    global data, online_user
     while True:
         try:
-            temp = conn.recv(1024).decode(encoding)
-            if not temp:
+            jmsg = conn.recv(1024).decode(encoding)
+            if not jmsg:
                 conn.close()
                 return
-            notify_all(temp)
-            print(data)
-
+            msg = json.loads(jmsg)
+            if msg['Object'] == 'all':
+                notify_all(msg)
         except:
-            notify_all(nick + ' 离开聊天室')
-            print(data)
+            del online_user[nick]
+            temp = {'Object': 'all', 'FromUser': 'system',
+                    'CreateTime': time.strftime("%H:%M:%S", time.localtime()), 'MsgType': 'text',
+                    'Content': nick + ' 离开聊天室', 'OnlineUser': list(online_user.keys())}
+            notify_all(temp)
             return
 
 
 def client_thread_out(conn, nick):
-    global data
+    global data, online_user
     while True:
-        if con.acquire():
-            con.wait()
-            if data:
-                try:
-                    conn.send(data.encode(encoding))
-                    con.release()
-                except:
-                    con.release()
-                    return
+        if nick not in online_user.keys():
+            return
+        if len(data[nick]) > 0 and data[nick][0]['ToUser'] == nick:
+            msg = data[nick][0].copy()
+            msg['OnlineUser'] = list(online_user.keys())
+            jmsg = json.dumps(msg) + '\n'
+            try:
+                conn.send(jmsg.encode(encoding))
+                threadLock.acquire()
+                data[nick].pop(0)
+                threadLock.release()
+            except:
+                return
 
 
-def notify_all(ss):
-    global data
-    if con.acquire():
-        data = ss
-        con.notifyAll()
-        con.release()
+def notify_all(msg):
+    for user in online_user.keys():
+        temp = msg.copy()
+        if user != msg['FromUser']:
+            temp['ToUser'] = user
+            data[user].append(temp)
 
 
-con = threading.Condition()
 HOST = ""
 PORT = 8964
-data = ''
+data = defaultdict(list)
 encoding = 'utf-8'
+online_user = {}  # { nick:{ ip:'', port:''}}
 
+threadLock = threading.Lock()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Socke建立')
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -59,10 +70,18 @@ print('开始监听端口')
 while True:
     conn, addr = s.accept()
     print(addr[0] + ':' + str(addr[1]) + " 连入服务器")
-    nick = conn.recv(1024).decode(encoding)
-    notify_all('欢迎 ' + nick + ' 进入聊天室！')
-    print(data)
+    try:
+        jnick = conn.recv(1024).decode(encoding)
+        nick = json.loads(jnick)['nick']
+    except:
+        continue
+    online_user[nick] = {}
+    online_user[nick]['ip'] = addr[0]
+    online_user[nick]['port'] = addr[1]
+    temp = {'Object': 'all', 'FromUser': 'system', 'ToUser': list(online_user.keys()),
+            'CreateTime': time.strftime("%H:%M:%S", time.localtime()), 'MsgType': 'text',
+            'Content': '欢迎 ' + nick + ' 进入聊天室！', 'OnlineUser': list(online_user.keys())}
+    notify_all(temp)
     print('当前 ' + str(int((threading.activeCount() + 1) / 2)) + ' 人在线')
-    conn.sendall(data.encode(encoding))
     threading.Thread(target=client_thread_in, args=(conn, nick)).start()
     threading.Thread(target=client_thread_out, args=(conn, nick)).start()
