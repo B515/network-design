4b2e3541f5dbde11da1c93507d7057323127af4e
WH_hunter <814188639@qq.com>
2018-06-24 19:47:23 +0800

    file\image

--- client.py
+++ client.py
@@ -4,10 +4,16 @@ import socket
 import threading
 import json
 import time
+import os
+from base64 import b64encode, b64decode
+from collections import defaultdict
+from random import randint
 
 outString = ''
 inString = ''
 nick = ''
+data_file = defaultdict(list)  # 待处理文件消息
+temp_file = defaultdict(dict)  # 临时存储文件
 encoding = 'utf-8'
 # ip = "123.207.164.148"  # 正式服务器
 ip = "123.206.13.211"  # 测试服务器
@@ -15,15 +21,38 @@ ip = "123.206.13.211"  # 测试服务器
 PORT = 8964
 
 
+class BytesEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, bytes):
+            return str(obj, encoding='utf-8')
+        return json.JSONEncoder.default(self, obj)
+
+
 def deal_out(sock):
     global nick, outString
     while True:
-        content = input()
-        outString = content
-        msg = {'Object': 'all', 'FromUser': nick, 'CreateTime': time.strftime("%H:%M:%S", time.localtime()),
-               'MsgType': 'text', 'Content': content}
-        jmsg = json.dumps(msg)
-        sock.send(jmsg.encode(encoding))
+        cmd = input("请输入类型{0：文字，1：图片， 2：文件}")
+        if cmd == '0':
+            content = input()
+            outString = content
+            msg = {'Object': 'all', 'FromUser': nick, 'CreateTime': time.strftime("%H:%M:%S", time.localtime()),
+                   'MsgType': 'text', 'Content': content}
+            jmsg = json.dumps(msg)
+            sock.send(jmsg.encode(encoding))
+        elif cmd == '1' or cmd == '2':
+            filename = input("请输入文件名：")
+            msg = {
+                'Object': 'all',  # 发送对象类型：all（群体）， personal（个人）
+                'FromUser': nick,  # 发送方姓名；必填
+                'CreateTime': time.strftime("%H:%M:%S", time.localtime()),  # 消息创建时间：形如23:59:59
+                'MsgType': 'image',  # 消息类型：image（图片）、file（文件）
+                'FileName': filename,  # 图片\文件名
+                'FileSize': os.stat(filename).st_size,  # 图片\文件大小
+                'MsgID': randint(10000000, 99999999)  # 本次传输id，随机8位数字，用于标记本次传输
+            }
+            jmsg = json.dumps(msg)
+            sock.send(jmsg.encode(encoding))
+            threading.Thread(target=deal_file, args=(sock, msg['MsgType'], msg['MsgID'], filename)).start()
 
 
 def deal_in(sock):
@@ -34,11 +63,58 @@ def deal_in(sock):
             if not jmsg:
                 break
             msg = json.loads(jmsg)
-            inString = msg['Content']
-            if outString != inString:
-                print('[' + msg['CreateTime'] + ']' + msg['FromUser'] + ':' + inString)
+            if msg['MsgType'] == 'text':
+                inString = msg['Content']
+                if outString != inString:
+                    print('[' + msg['CreateTime'] + ']' + msg['FromUser'] + ':' + inString)
+            elif msg['MsgType'] == 'image' or msg['MsgType'] == 'file':
+                if msg['MsgID'] not in temp_file.keys():
+                    temp_file[msg['MsgID']] = msg
+                    data_file[msg['MsgID']] = []
+                    threading.Thread(target=deal_file_in, args=(msg,)).start()
+                else:
+                    data_file[msg['MsgID']].append(msg)
         except:
             break
+
+
+def deal_file(sock, msg_type, f_id, filename):
+    fp = open(filename, 'rb')
+    while True:
+        data = b64encode(fp.read(512)).decode(encoding)
+        if not data:
+            print("发送完毕")
+            break
+        msg = {
+            'MsgType': msg_type,  # 消息类型：image（图片）、file（文件）
+            'MsgID': f_id,  # 本次传输id，8位数字，需与首次发送id相同
+            'Content': data  # 图片\文件内容，每次最大传输960
+        }
+        jmsg = json.dumps(msg)
+        sock.send(jmsg.encode(encoding))
+        time.sleep(0.05)
+    fp.close()
+    return
+
+
+def deal_file_in(msg):
+    global temp_file, data_file
+    recv_size = 0
+    filename = os.path.join('./recv/', msg['FileName'])
+    fp = open(filename, 'wb')
+    while not recv_size == msg['FileSize']:
+        if len(data_file[msg['MsgID']]) > 0:
+            temp = b64decode(data_file[msg['MsgID']][0]['Content'])
+            if msg['FileSize'] - recv_size > 512:
+                recv_size += len(temp)
+            else:
+                recv_size = msg['FileSize']
+            fp.write(temp)
+            data_file[msg['MsgID']].pop(0)
+    fp.close()
+    del temp_file[msg['MsgID']]
+    del data_file[msg['MsgID']]
+    return
 
 
 nick = input('请输入昵称：')
--- server.py
+++ server.py
@@ -4,7 +4,9 @@ import socket
 import threading
 import json
 import time
+import os
 from collections import defaultdict
+from base64 import b64encode, b64decode
 
 
 def client_thread_in(conn, nick):
@@ -16,11 +18,19 @@ def client_thread_in(conn, nick):
                 conn.close()
                 return
             msg = json.loads(jmsg)
-            print('[' + msg['CreateTime'] + ']' + msg['FromUser'] + ':' + msg['Content'])
-            if msg['Object'] == 'all':
-                notify_all(msg)
-            elif msg['Object'] == 'personal':
-                data[nick].append(msg)
+            if msg['MsgType'] == 'text':
+                print('[' + msg['CreateTime'] + ']' + msg['FromUser'] + ':' + msg['Content'])
+                if msg['Object'] == 'all':
+                    notify_all(msg)
+                elif msg['Object'] == 'personal':
+                    data[msg['ToUser']].append(msg)
+            elif msg['MsgType'] == 'image' or msg['MsgType'] == 'file':
+                if msg['MsgID'] not in temp_file.keys():
+                    temp_file[msg['MsgID']] = msg
+                    data_file[msg['MsgID']] = []
+                    threading.Thread(target=deal_file, args=(msg['MsgID'], msg['FileSize'])).start()
+                else:
+                    data_file[msg['MsgID']].append(msg)
         except:
             del online_user[nick]
             temp = {'Object': 'all', 'FromUser': 'system',
@@ -36,20 +46,32 @@ def client_thread_out(conn, nick):
     while True:
         if nick not in online_user.keys():
             return
-        if len(data[nick]) > 0 and data[nick][0]['ToUser'] == nick:
+        if len(data[nick]) > 0:
             msg = data[nick][0].copy()
             msg['OnlineUser'] = list(online_user.keys())
-            jmsg = json.dumps(msg) + '\n'
-            try:
-                conn.send(jmsg.encode(encoding))
-                threadLock.acquire()
-                data[nick].pop(0)
-                threadLock.release()
-            except:
-                return
+            if msg['MsgType'] == 'text':
+                jmsg = json.dumps(msg) + '\n'
+                try:
+                    conn.send(jmsg.encode(encoding))
+                    threadLock.acquire()
+                    data[nick].pop(0)
+                    threadLock.release()
+                except:
+                    return
+            elif msg['MsgType'] == 'image' or msg['MsgType'] == 'file':
+                jmsg = json.dumps(msg)
+                try:
+                    conn.send(jmsg.encode(encoding))
+                    threading.Thread(target=deal_file_out, args=(conn, msg['MsgID'], msg['MsgType'])).start()
+                    threadLock.acquire()
+                    data[nick].pop(0)
+                    threadLock.release()
+                except:
+                    return
 
 
 def notify_all(msg):
+    global data
     for user in online_user.keys():
         temp = msg.copy()
         if user != msg['FromUser']:
@@ -57,9 +79,54 @@ def notify_all(msg):
             data[user].append(temp)
 
 
+def deal_file(f_id, f_size):
+    global temp_file, data_file, data
+    recv_size = 0
+    filename = os.path.join('./temp/', str(f_id))
+    fp = open(filename, 'wb')
+    while not recv_size == f_size:
+        if len(data_file[f_id]) > 0:
+            temp = b64decode(data_file[f_id][0]['Content'])
+            if f_size - recv_size > 512:
+                recv_size += len(temp)
+            else:
+                recv_size = f_size
+            fp.write(temp)
+            data_file[f_id].pop(0)
+    fp.close()
+    if temp_file[f_id]['Object'] == 'all':
+        notify_all(temp_file[f_id])
+    elif temp_file[f_id]['Object'] == 'personal':
+        data[temp_file[f_id]['ToUser']].append(temp_file[f_id])
+    del temp_file[f_id]
+    del data_file[f_id]
+    return
+
+
+def deal_file_out(conn, msg_id, msg_type):
+    fp = open("./temp/" + str(msg_id), "rb")
+    while True:
+        data = b64encode(fp.read(512)).decode(encoding)
+        if not data:
+            print("发送完毕")
+            break
+        msg = {
+            'MsgType': msg_type,  # 消息类型：image（图片）、file（文件）
+            'MsgID': msg_id,  # 本次传输id，8位数字，需与首次发送id相同
+            'Content': data  # 图片\文件内容，每次最大传输960
+        }
+        jmsg = json.dumps(msg)
+        conn.send(jmsg.encode(encoding))
+        time.sleep(0.05)
+    fp.close()
+    return
+
+
 HOST = ""
 PORT = 8964
 data = defaultdict(list)
+data_file = defaultdict(list)  # 待处理文件消息
+temp_file = defaultdict(dict)  # 临时存储文件
 encoding = 'utf-8'
 online_user = {}  # { nick:{ ip:'', port:''}}
 
